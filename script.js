9a6296f7d6e464c1a33f0af06632638d83e86aef
kcc666 <463217787@qq.com>
2025-02-19 21:12:44 +0800

    1
    1

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ let clickCount = 0;  // 记录点击 No 的次数
 
 // No 按钮的文字变化
 const noTexts = [
-    "？你认真的吗…", 
-    "要不再想想？", 
-    "不许选这个！ ", 
-    "我会很伤心…", 
-    "不行:("
+    "对不起，我知道错了", 
+    "零花钱都给你", 
+    "欲擒故纵，我懂！", 
+    "能不能给个台阶嘛", 
+    "原谅我嘛，对不起"
 ];
 
 // No 按钮点击事件
@@ -37,10 +37,10 @@ noButton.addEventListener("click", function() {
     }
 
     // 图片变化（前 5 次变化）
-    if (clickCount === 1) mainImage.src = "images/shocked.png"; // 震惊
-    if (clickCount === 2) mainImage.src = "images/think.png";   // 思考
-    if (clickCount === 3) mainImage.src = "images/angry.png";   // 生气
-    if (clickCount === 4) mainImage.src = "images/crying.png";  // 哭
+    if (clickCount === 1) mainImage.src = "images/rencuo.png"; // 震惊
+    if (clickCount === 2) mainImage.src = "images/geiqian.png";   // 思考
+    if (clickCount === 3) mainImage.src = "images/duoqilai.png";   // 生气
+    if (clickCount === 4) mainImage.src = "images/weiqu.png";  // 哭
     if (clickCount >= 5) mainImage.src = "images/crying.png";  // 之后一直是哭
 
 });
